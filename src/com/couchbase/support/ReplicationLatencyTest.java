5a8c0aa202c9127c8354eae2b25d62a20bc041a6
couchbasebrian <brian.williams@couchbase.com>
2015-06-11 15:41:21 -0700

    Many improvements

--- src/com/couchbase/support/ReplicationLatencyTest.java
+++ src/com/couchbase/support/ReplicationLatencyTest.java
@@ -21,8 +21,8 @@ public class ReplicationLatencyTest {
 			
 		// Define two clusters, a source and a target ( local and remote )
 		
-		String sourceClusterAddress = "192.168.0.101", sourceBucketName = "beer-sample",
-		       targetClusterAddress = "192.168.0.102", targetBucketName = "beer-sample";
+		String sourceClusterAddress = "10.4.2.121", sourceBucketName = "default",
+		       targetClusterAddress = "10.4.2.123", targetBucketName = "default";
 				
 		// Connect to both clusters
 		CouchbaseCluster sourceCluster = CouchbaseCluster.create(sourceClusterAddress);
@@ -36,7 +36,13 @@ public class ReplicationLatencyTest {
 		logMessage("Finished opening target clusters and buckets");
 		
 		// Create an expiring document on the source cluster
-		String documentKey = "testDocument2";
+		int randomIdentifier = (int) (Math.random() * 1000);
+		String documentKey = "testDocument" + randomIdentifier;
+
+		long iterationsRequired = 0;
+		
+		logMessage("Ready to perform tests - document id is " + documentKey);
+		
 		String jsonDocumentString = "{ \"name\" : \"testDocument\" }";
 		JsonObject jsonObject = JsonObject.fromJson(jsonDocumentString);
 		int timeToLive = 3; // seconds for document to live
@@ -44,8 +50,10 @@ public class ReplicationLatencyTest {
 		long timeOut = 1000; // 1 second timeout value for the insert operation only 
 		TimeUnit timeUnit = TimeUnit.MILLISECONDS;
 		sourceBucket.insert(jsonDocument, timeOut, timeUnit);
-		
-		logMessage("Finished inserting document");
+
+		long timeWhenDocumentFirstInsertedIntoSourceBucket = System.currentTimeMillis();		
+
+		logMessage("Finished inserting document into source bucket");
 		
 		long t1 = 0, t2 = 0, timeToAppear = 0, timeToAppear2 = 0, timeToDisappear = 0, 
 				timeForCasChange = 0, timeToBeRemoved = 0;
@@ -54,43 +62,85 @@ public class ReplicationLatencyTest {
 		
 		logMessage("Waiting for doc to appear in target bucket...");
 		
+		iterationsRequired = 0;
 		t1 = System.currentTimeMillis();		
 		// Poll the target cluster until the doc appears 
 		boolean docFound = false;
 		while (docFound == false) {
 			foundDocument = targetBucket.get(documentKey);
-			if (foundDocument != null) { docFound = true; }
+			if (foundDocument != null) { 
+				docFound = true; 
+			}
+			else {
+				iterationsRequired++;				
+			}
 		}
 		t2 = System.currentTimeMillis();
 		timeToAppear = t2 - t1;
+		logMessage("Iterations required: " + iterationsRequired);
 		
 		logMessage("Waiting for doc to disappear from target bucket... (expecting about " + timeToLive + " seconds)");
 
 		// and disappears
+		iterationsRequired = 0;
 		t1 = System.currentTimeMillis();		
 		docFound = true;
 		while (docFound == true) {
 			foundDocument = targetBucket.get(documentKey);			
-			if (foundDocument == null) { docFound = false; }
+			if (foundDocument == null) { 
+				docFound = false; 
+			} else {
+				iterationsRequired++;
+			}
 		}
 		t2 = System.currentTimeMillis();
 		timeToDisappear = t2 - t1;
-	
-		// Re-create the document again in the source bucket
-		sourceBucket.insert(jsonDocument, timeOut, timeUnit);
-
-		logMessage("Waiting for doc to appear in target bucket again...");
+		logMessage("Iterations required: " + iterationsRequired);
 		
+		logMessage("Doc has disappeared from target bucket.  Inserting document into source bucket again ( assuming it has expired )");
+		
+		boolean exceptionUponReinsert = false;
+		boolean successfulReinsert = false;
+		iterationsRequired = 0;
+		t1 = System.currentTimeMillis();
+		while (successfulReinsert == false) {
+			try {
+				// Re-create the document again in the source bucket
+				sourceBucket.insert(jsonDocument, timeOut, timeUnit);
+				successfulReinsert = true;
+			}
+			catch (Exception e) {
+				iterationsRequired++;		
+				exceptionUponReinsert = true;
+			}
+		}
+		t2 = System.currentTimeMillis();
+		long timeToReinsert = t2 - t1;
+		logMessage("Iterations required: " + iterationsRequired);
+
+		if (exceptionUponReinsert) {
+			logMessage("There were exceptions at reinsert.  The total life time in the source bucket was " + 
+						(t2 - timeWhenDocumentFirstInsertedIntoSourceBucket) 
+						+ " ms.");
+		}
+		
+		logMessage("Reinsert into source bucket successful (took " + timeToReinsert + " ms).  Waiting for doc to appear in target bucket again...");
+		
+		iterationsRequired = 0;
 		t1 = System.currentTimeMillis();		
 		// Poll the target cluster until the doc re-appears in the target bucket. 
 		docFound = false;
 		while (docFound == false) {
 			foundDocument = targetBucket.get(documentKey);
-			if (foundDocument != null) { docFound = true; }
+			if (foundDocument != null) { docFound = true; } else { iterationsRequired++; }
 		}
 		t2 = System.currentTimeMillis();
 		timeToAppear2 = t2 - t1;
+		logMessage("Iterations required: " + iterationsRequired);
 
+		logMessage("The document appeared in the target bucket after being reinserted into the source bucket.");
+		logMessage("Now doing an upsert on the document in the source bucket.");
+		
 		// Change the doc with an upsert to the source bucket
 		// and see how long it takes for that to propagate to target bucket
 
@@ -98,15 +148,20 @@ public class ReplicationLatencyTest {
 		JsonObject newJsonObject = JsonObject.fromJson(newJsonDocumentString);
 		JsonDocument newJsonDocument = JsonDocument.create(documentKey, timeToLive, newJsonObject); // same key and TTL
 
+		t1 = System.currentTimeMillis();		
 		sourceBucket.upsert(newJsonDocument, timeOut, timeUnit);
-		logMessage("Finished upserting document with key: " + documentKey + " cas: " + newJsonDocument.cas());
+		t2 = System.currentTimeMillis();		
+		logMessage("Finished upserting document with key: " + documentKey + 
+				" CAS: " + newJsonDocument.cas() + 
+				" timeTaken: " + (t2 - t1) + " ms");
 
 		JsonDocument retrievedChangedDocument = sourceBucket.get(documentKey);	
 		long retrievedDocumentCas = retrievedChangedDocument.cas();
-		logMessage("Retrieved the upserted doc and the cas is: " + retrievedDocumentCas);
+		logMessage("Retrieved the upserted doc from the source bucket and the CAS is: " + retrievedDocumentCas);
 
 		logMessage("Waiting for doc to change in target bucket...");
 
+		iterationsRequired = 0;
 		t1 = System.currentTimeMillis();		
 		// Poll the target cluster until the cas changes 
 		boolean casChanged = false;
@@ -123,30 +178,37 @@ public class ReplicationLatencyTest {
 					System.out.print("x");
 				}
 			}
+			iterationsRequired++;
 		}
 		t2 = System.currentTimeMillis();
 		timeForCasChange = t2 - t1;
 		System.out.println();
-		// System.out.println("Retrieved upserted doc from target cluster: " + foundDocument.toString());
-
+		logMessage("Iterations required: " + iterationsRequired);
+		
+		logMessage("Detected the CAS change on the target bucket.  Doing a remove on the source bucket and polling target bucket...");
+		
 		// Remove the document from the source bucket and see how long it takes to disappear
 		// from target bucket
 		sourceBucket.remove(documentKey);
 		
+		iterationsRequired = 0;
 		t1 = System.currentTimeMillis();		
 		docFound = true;
 		while (docFound == true) {
 			foundDocument = targetBucket.get(documentKey);			
-			if (foundDocument == null) { docFound = false; }
+			if (foundDocument == null) { docFound = false; } else { iterationsRequired++; }
 		}
 		t2 = System.currentTimeMillis();
 		timeToBeRemoved = t2 - t1;
+		logMessage("Iterations required: " + iterationsRequired);
+		
+		logMessage("The document has disappeared from the target bucket.");
 		
 		// Display final results
 		logMessage("Final Results");
 		
 		System.out.println("Time taken to appear in target bucket:                       " + timeToAppear     + " ms");
-		System.out.println("Time taken to expire and disappear from target bucket:       " + timeToDisappear  + " ms");
+		System.out.println("Time taken to expire and disappear from target bucket:       " + timeToDisappear  + " ms after appearing in target");
 		System.out.println("Time taken to reappear in target after recreating in source: " + timeToAppear2    + " ms");		
 		System.out.println("Time for upsert to propagate to target bucket:               " + timeForCasChange + " ms");
 		System.out.println("Time taken for remove to take effect on target bucket:       " + timeToBeRemoved  + " ms");
